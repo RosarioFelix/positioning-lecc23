e203e9f8d7fcde1c235ffa5e279b142e3f105351
Rosario Felix <elijan_11@hotmail.com>
2017-04-18 15:56:04 -0500

    subiendo por JS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
-window.addEventListener(load,function) {
+window.addEventListener("load",function() {
 
-};
+  var contenedor=document.getElementsByClassName('contenedor')[0];
+  var rosa= document.getElementById('rosa');
+  var azul=document.getElementById("azul");
+  var verde = document.getElementById('verde');
+  var amarillo=document.getElementById('amarillo');
+
+  //poniendole propiedades a contenedor
+
+  contenedor.style.position="relative";
+  contenedor.style.background="DarkGray";
+  contenedor.style.width="600px";
+  contenedor.style.padding="50px";
+
+  azul.style.position="absolute";
+  azul.style.top="250px";
+  azul.style.right="0";
+});
